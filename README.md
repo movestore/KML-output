fb367a0953e8070db738ceaea2c5075c4fa04e7b
Andrea Kölzsch <65662928+andreakoelzsch@users.noreply.github.com>
2021-05-11 13:01:30 +0200

    first version

--- README.md
+++ README.md
@@ -1,1 +1,30 @@
-# KML-output
+# KML Map Output
+
+MoveApps
+
+Github repository: *github.com/movestore/KML-output*
+
+## Description
+A small App to explore your tracks with kml files. Each track is presented as a line and individual locations can be laid on top.
+
+## Documentation
+Using the R-package `plotKML` this App creates for each track a kml file of the locations as points and one overview kml of all tracks as lines (in colours by individual).
+
+Download the files and open them locally in Google Earth. They can be opened together in Google Earth for overlaying and better exploration. A timeslider for the point files allows to show only points of a specified time interval.
+
+### Input data
+moveStack in Movebank format
+
+### Output data
+moveStack in Movebank format
+
+### Artefacts
+`ptsID.kml`: google earth kml file for each individual track. The locations are presented as points with underlying information about ID and timestamp. A timeslider allows to show only part of the points. The file(s) can be opened together with `trajectories.kml` in Google Earth for overlay and data exploration.
+
+`trajectories.kml`: google earth kml file of all individual tracks as lines. Each track has a different colour and can be disabled from view in google earth.
+
+### Parameters 
+none.
+
+### Null or error handling:
+**Data:** The data are not manipulated, but empty input with no locations (NULL) leads to an error. For calculations in further Apps the input data set is returned.
---
+++ RFunction.R
@@ -0,0 +1,89 @@
+library('move')
+library('spacetime')
+library('plotKML')
+
+rFunction <- function(data)
+{
+  Sys.setenv(tz="GMT")
+  
+  ###  create space time object
+  sp_dat <- SpatialPoints(coordinates(data),proj4string=CRS("+proj=longlat +ellps=WGS84 +datum=WGS84 +towgs84=0,0,0"))
+  sp_temp_all <- STIDF(sp_dat, timestamps(data), as.data.frame(data))
+  
+  # prepare html.table
+  sp_temp_all@data$date_time_attr <- paste("DateTime:", sp_temp_all@data$timestamp, sep = " ")
+  sp_temp_all@data$ID_attr <- paste("ID:", sp_temp_all@data$trackId, sep = " ")
+  
+  # kml
+  shape <- "http://maps.google.com/mapfiles/kml/paddle/stop-lv.png" #like this or with file in MOveApps?
+  
+  
+  ids <- as.character(unique(sp_temp_all@data$trackId))
+
+  for (i in seq(along=ids)) 
+  {
+    sp_temp_single_ID <- sp_temp_all[sp_temp_all@data$trackId == ids[i],]
+
+    attributes_sub <- paste(sp_temp_single_ID@data$ID_attr, sp_temp_single_ID@data$date_time_attr, sep = "; ")
+
+    # night roost points 
+    kml_open(sprintf("pts%s.kml", ids[i]) )
+    
+    kml_layer(sp_temp_single_ID,
+              shape = shape, 
+              points_names = "",
+              size = 0.7,
+              match.ID = FALSE, 
+              html.table = attributes_sub)
+    
+    kml_close(sprintf("pts%s.kml", ids[i]) )
+    
+    sp_temp_single_ID <- vector()
+    
+  }
+  
+  
+  #################################################
+  #### create trajectories - add lines
+  
+  all_lines <- name_info <- list()
+  data.split <- move::split(data)
+  
+  for (j in seq(along=ids)) {
+    datai <- data.split[[j]]
+    line_class <- Line(coordinates(datai))
+    
+    all_lines[[j]] <- Lines(line_class, ID=ids[j])
+    name_info[[j]] <- ids[j]
+    
+    line_class <- vector()
+  }
+  
+  name_info_df <- as.data.frame(unlist(name_info))
+  names(name_info_df) <- "ID"           
+  
+  row.names(name_info_df) <- name_info_df$ID
+  
+  spLin_dat <- SpatialLines(all_lines)
+  spLindf_dat <- SpatialLinesDataFrame(spLin_dat, name_info_df)
+  proj4string(spLindf_dat) <- CRS("+proj=longlat +ellps=WGS84 +datum=WGS84 +towgs84=0,0,0")
+  
+  
+  kml_open("trajectories.kml")
+  kml_layer.SpatialLines(spLindf_dat, subfolder.name = paste(class(spLindf_dat)), colour=ID, width=3)
+  kml_close("trajectories.kml")
+  
+  result <- data
+  return(result)
+}
+
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
