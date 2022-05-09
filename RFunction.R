01e9d01e153ccc475c671ffa3fdb0d6a30bb19eb
Andrea Kölzsch <65662928+andreakoelzsch@users.noreply.github.com>
2022-05-09 15:38:33 +0200

    adapt point icon

--- RFunction.R
+++ RFunction.R
@@ -1,6 +1,7 @@
 library('move')
 library('spacetime')
 library('plotKML')
+library('plotrix')
 
 rFunction <- function(data)
 {
@@ -15,22 +16,24 @@ rFunction <- function(data)
   sp_temp_all@data$ID_attr <- paste("ID:", sp_temp_all@data$trackId, sep = " ")
   
   # kml
-  shape <- "http://maps.google.com/mapfiles/kml/paddle/stop-lv.png" #like this or with file in MOveApps?
-  
+  shape <- "http://maps.google.com/mapfiles/kml/pal2/icon18.png" #like this or with file in MOveApps?
   
   ids <- as.character(unique(sp_temp_all@data$trackId))
+  #col <- sapply(rainbow(n=length(ids)),color.id)
 
+  
   for (i in seq(along=ids)) 
   {
     sp_temp_single_ID <- sp_temp_all[sp_temp_all@data$trackId == ids[i],]
-
+    
     attributes_sub <- paste(sp_temp_single_ID@data$ID_attr, sp_temp_single_ID@data$date_time_attr, sep = "; ")
 
     # night roost points 
     kml_open(paste0(Sys.getenv(x = "APP_ARTIFACTS_DIR", "/tmp/"),sprintf("pts%s.kml", ids[i])))
     #kml_open(sprintf("pts%s.kml", ids[i]) )
     
-    kml_layer(sp_temp_single_ID,
+    kml_layer(sp_temp_single_ID, 
+              colour="red",#cannot make this colour variable with parameter col[i]
               shape = shape, 
               points_names = "",
               size = 0.7,
@@ -50,13 +53,14 @@ rFunction <- function(data)
   
   all_lines <- name_info <- list()
   data.split <- move::split(data)
+  ids_l <- names(data.split)
   
-  for (j in seq(along=ids)) {
+  for (j in seq(along=ids_l)) {
     datai <- data.split[[j]]
     line_class <- Line(coordinates(datai))
     
-    all_lines[[j]] <- Lines(line_class, ID=ids[j])
-    name_info[[j]] <- ids[j]
+    all_lines[[j]] <- Lines(line_class, ID=ids_l[j])
+    name_info[[j]] <- ids_l[j]
     
     line_class <- vector()
   }
